0147a9d3ab0f1730f2f52e873a740efd263910f2
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-03-16 14:40:12 +0200

    lesson 1
    lesson 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { RecipeList } from "./RecipeList";
+import recipes from "../components/recipes.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template 1
+    <div>
+      <RecipeList items={recipes} />
     </div>
   );
 };
